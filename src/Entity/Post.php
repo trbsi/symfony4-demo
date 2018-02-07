d7e5f7ef7eac891b80a61b096d262fe928e1eff0
Unknown <timmy.dario@gmail.com>
2018-02-07 18:53:59 +0100

    Testing manytomany joining table

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -115,7 +115,13 @@ class Post
      * @var Tag[]|ArrayCollection
      *
      * @ORM\ManyToMany(targetEntity="App\Entity\Tag", cascade={"persist"})
-     * @ORM\JoinTable(name="symfony_demo_post_tag")
+     * @ORM\JoinTable(name="symfony_demo_post_tag",
+     * joinColumns={
+     *     @ORM\JoinColumn(name="post_id", referencedColumnName="id")
+     * },
+     * inverseJoinColumns={
+     *     @ORM\JoinColumn(name="tag_id", referencedColumnName="id")
+     * })
      * @ORM\OrderBy({"name": "ASC"})
      * @Assert\Count(max="4", maxMessage="post.too_many_tags")
      */
